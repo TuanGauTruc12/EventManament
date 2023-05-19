5c50358c7114d67b50dfe0272e90ac35b20f2f47
TuanGauTruc12 <phamtuan12022001@gmail.com>
2023-05-19 11:33:07 +0700

    commit

--- src/components/EventItem.jsx
+++ src/components/EventItem.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const EventItem = () => {
+  const navvigate = useNavigate();
+  const handleEventItem = () => {
+    navvigate("/service");
+  };
+
   return (
     <div className="item-event">
       <img
         src="https://vapa.vn/wp-content/uploads/2022/12/anh-dep-lam-hinh-nen-002.jpg"
         alt="img"
+        onClick={handleEventItem}
       />
       <div className="flex flex-col gap-1">
         <span>Sự kiện 1</span>
